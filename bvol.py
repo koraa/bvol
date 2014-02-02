01e84b5490d0532591f96e7c3272348a6e5e9c8e
Karolin Varner <karo@cupdev.net>
2014-02-02 18:38:39 +0100

    Get ls command working

--- bvol.py
+++ bvol.py
@@ -20,7 +20,8 @@ import os.path as path
 import subprocess as sub
 import itertools as itr
 reject = itr.filterfalse
-flat1 = itr.chain.from_iterable
+chain1 = itr.chain.from_iterable
+ser = sys.stderr
 
 #######################################################
 # UTILS
@@ -39,13 +40,16 @@ def empty(iterbl):
     """Check if a string is empty"""
     return len(iterbl) == 0
 
-def join(*argv, **A):
+def join1(argv, **A):
     """Easy join: Take multiple arguments instead of a list,
     supports the flag delm, which specifies the delimiter.
     delm is by default ' '."""
     delm = flag(A, "delm", " ")
     segs = map(str, argv)
-    return selm.join(segs)
+    return delm.join(segs)
+
+def join(*argv, **A):
+    return join1(argv, **A)
 
 def fill(l, no, default=None):
     """Append so many $defaults to l that it has at least no
@@ -61,7 +65,7 @@ def ttystr(b):
     """Convert bytes from console to a string"""
     # TODO: This should care about the locale
     if isinstance(b, bytes):
-        return str(path, encoding='utf-8')
+        return str(b, encoding='utf-8')
     else:
         return b
 
@@ -91,14 +95,27 @@ def compatl(l):
     None values."""
     return (v for v in l if v != None)
 
+def head(l):
+    """Head of a list.
+    First element only."""
+    return l[0]
+
 def tail(l):
     """Tail of a list.
     All elements except the first."""
     return l[1:]
 
+def last(l):
+    """Last element of a list"""
+    return l[-1]
+
+def initial(l):
+    """All elements of a list except the last one"""
+    return l[0:-1]
+
 def XOR(*arg):
     """Boolean XOR. Inverse of XNOR.
-    Takes any number of arguments. Will return true, if the
+    Takes any number of arguments. Will return True, if the
     number of truthy arguments is odd. For two arguments
     this is equal to `bool(a) != bool(b)`."""
     # This works because True<=>1, False<=>0
@@ -111,6 +128,21 @@ def XNOR(*arg):
     this is equivalent to `bool(a) == bool(b)`."""
     return not XOR(*arg)
 
+def ls_dirs(d):
+    """List all the subdirectories names in the given dir"""
+    return os.walk(d).__next__()[1]
+
+def prep(*args):
+    """Prepends a number of elements to an iterable.
+    All the initial arguments are prepended to the last one.
+    Returns a new iterable."""
+    return itr.chain(initial(args), last(args))
+
+def app(l, *a):
+    """Appends a number of elements to the given list.
+    Returns a new iterable"""
+    return itr.chain(l,a)
+
 #######################################################
 # BTRFS
 
@@ -121,7 +153,7 @@ class __BtrfsListEntry:
     def __init__(self, line):
         [_, id, _,  generation, _, _, level,
                 _, path] = line.split()
-        self.path = ttystr(path)
+        self.path = path
         self.id = int(id)
         self.generation = int(generation)
 
@@ -133,6 +165,7 @@ def btrfs_list(mnt):
             stderr=sys.stderr, stdin=sys.stdin)
 
     x= iter(proc.stdout.readline, b'')
+    x= map(ttystr, x)
     x= map(__BtrfsListEntry, x)
     x= ( (e.path, e) for e in x)
     return dict(x)
@@ -141,23 +174,23 @@ class BVolAssertError(IOError):
     pass
 
 class BVol:
+    @staticmethod
     def fromPathInPrefix(prefix, pa):
         pool, subvol = fill(pa.split("/", 1), 2, '')
-        self.fromPathInPool(rppt, pool, subvol)
+        return BVol.fromPathInPool(prefix, pool, subvol)
 
     # TODO: Use [path]
     @staticmethod
     def fromPathInPool(prefix, pool, subvol):
         pathar = subvol.split('/')
-        atsegs = pathar[-1].split("@")
+        atsegs = last(pathar).split("@")
         if len(atsegs) > 1:
             snap = atsegs.pop()
-            name = join(atsegs, '')
+            name = join1(atsegs, delm="@")
             pathar[-1] = name
-            return BVol(prefix, pool, join(pathar, '/'), snap)
+            return BVol(prefix, pool, join1(pathar, delm='/'), snap)
         else:
             return BVol(prefix, pool, subvol)
-    
 
     def __init__(self, prefix, pool='', subvol='', snapname=''):
         """
@@ -172,11 +205,23 @@ class BVol:
                    snapshot. Mus
         """
         self.prefix = prefix
-        self.vol = vol
+        self.pool = pool
         self.subvol = subvol
+        self.snapname = snapname
 
     def __str__(self):
-        return self.pathin_fs()
+        type = None
+        if self.is_prefix():
+            return "PREFIX ! " + self.pathin_fs()
+
+        if self.is_snap():
+            type = "SNAP"
+        elif self.is_pool():
+            type = "POOL"
+        elif self.is_subvol():
+            type = "VOL "
+
+        return type + " " + self.pathin_prefix()
 
     def __eq__(self, other):
         return self is other \
@@ -189,23 +234,21 @@ class BVol:
     def pathin_fs(self):
         """Returns the absolute path of this volume in the
         prefix fs."""
-        return path.join(prefix, pool, subvol + self.__snapsuf())
+        return path.join(
+                self.prefix, self.pool,
+                self.subvol + self.__snapsuf())
 
     def pathin_pool(self):
         """Returns the path of this subvol relative to the
         pool. Will return .' if this is a subvol or prefix."""
-        return subvol + self.__snapsuf()
+        return self.subvol + self.__snapsuf()
 
     def pathin_prefix(self):
         """Returns the path of this subvolume relative to
         the pool. Returns ''  if this is a prefix."""
-        return pool + self.pathin_pool()
-
-
-    def snapname(self):
-        """Returns the name of the snapshot or '' if this is
-        not a snapshot."""
-        return snaname
+        return path.join(
+                self.pool,
+                self.pathin_pool())
 
     def volname(self):
         """Returns the name of this subvolume, not including
@@ -221,7 +264,7 @@ class BVol:
         This eases implementation of some functions here but
         it is not such a good interface for outside..."""
         if self.is_snap():
-            return '@' + self.snapname()
+            return '@' + self.snapname
         else:
             return ''
 
@@ -234,7 +277,7 @@ class BVol:
         return path.exists(self.pathin_fs)
 
     def is_pool(self):
-        return not empty(self.pool) and     empty(self.subvo)
+        return not empty(self.pool) and empty(self.subvol)
 
     def is_subvol(self):
         return not empty(self.pool) and not empty(self.subvol)
@@ -243,7 +286,7 @@ class BVol:
         return     empty(self.pool) and     empty(self.subvol)
 
     def is_snap(self):
-        return empty(self.snapname)
+        return not empty(self.snapname)
 
     def _ver_(self, boll, msg,
             action="--", expect=True, skip=False):
@@ -349,17 +392,32 @@ class BVol:
             cand= [ v for v in cand if v == self ]
             return cand
 
-    def childs(self, **A):
-        """Returns all the childs of this subvolume as an
-        array. If the flag 'recursive' is set the list will
-        also contain children of children (of children...)."""
+    def __childs_PREFIX(self, **A):
+        """List the childs of a prefix. Used as a helper by
+        childs."""
+        pools= ls_dirs(self.pathin_fs())
+        pools= (BVol(self.prefix, n) for n in pools)
+
+        if flag(A, "recursive"):
+            subs = (pol.subtree(recursive=True) for pol in pools)
+            return chain1(subs)
+        else:
+            return pools
+
+    def __childs_VOl_POOL(self, **A):
+        """List the childs of a volume or pool. Used as
+        a helper by childs."""
         # We use the pool path here to get the benefits of
         # caching in btrfs_list
         l= btrfs_list(self.get_pool().pathin_fs())
         l= l.keys() # => list of subvol paths
 
-        l= (v for v in l if
-                v.startswith(self.pathin_pool() + "/" ))
+        if self.is_subvol():
+            # Filter every pool that is not below this, by
+            # checking the prefix path within the pool.
+            # Not necessary & not working for pools
+            l= (v for v in l if
+                    v.startswith(self.pathin_pool() + "/" ))
 
         if not flag(A, "recursive", False):
             # Filter deeper levels by checking for '/' in
@@ -367,11 +425,21 @@ class BVol:
             prefl = len(self.pathin_pool()) + 1
             l= (v for v in l if ( "/" not in v[prefl:]) )
 
-        return (BVol.fromSubvol(self.prefix, self.pool, v) for v in l)
+        l= (BVol.fromPathInPool(self.prefix, self.pool, v) for v in l)
+        return l
+
+    def childs(self, **A):
+        """Returns all the childs of this  as an array. If
+        the flag 'recursive' is set the list will also
+        contain children of children (of children...)."""
+        if (self.is_prefix()):
+            return self.__childs_PREFIX(**A)
+        else:
+            return self.__childs_VOl_POOL(**A)
 
     def __subtree_ALL(self, **A):
         """Helper for subtree: the entire subtree."""
-        return [self] + self.childs(**A)
+        return prep(self, self.childs(**A))
 
     def __subtree_only_VOLS(self, **A):
         if self.is_pool():
@@ -382,19 +450,19 @@ class BVol:
     def subtree(self, **A):
         """Like childs, but also includes SELF.
         Supports the only='vols' argument to exclude
-        pools and prefixs."""
+        pools and prefixes."""
         only = default(A, 'only')
 
         if only == 'vols':
             B=A.copy()
             B.pop("only", None)
-            return __subtree_only_VOLS(**B)
+            return self.__subtree_only_VOLS(**B)
         else:
-            return __subtree_ALL(**A)
+            return self.__subtree_ALL(**A)
 
     def __do_clone_REC(self, targ, **A):
         """Helper function for recursive cloning.
-        Use do_clone(recurisiv=true, ...) instead!"""
+        Use do_clone(recurisiv=True, ...) instead!"""
 
         B=A.copy().update({'recursive': False})
         # First clone this.
@@ -431,13 +499,13 @@ class BVol:
         if flag(A, 'readonly'):
             params.append('-r')
         
-        cwd=flag(A, 
-                'relative_to', 
+        cwd=flag(A,
+                'relative_to',
                 self.cwd())
 
         proc = sub.Popen(
-                ['btrfs', 'subvolume', 'snapshot', 
-                    self.pathin_fs(), 
+                ["echo", 'btrfs', 'subvolume', 'snapshot',
+                    self.pathin_fs(),
                     './' + targ] + params,
                 stdout = sys.stdout, stderr = sys.stderr,
                 stdin = sys.stdin,
@@ -480,7 +548,7 @@ class BVol:
 
         delargs = (v.pathin_pool() for v in todel)
         proc = sub.Popen(
-                ['btrfs', 'subvolume', 'destroy'] + delargs,
+                ["echo", 'btrfs', 'subvolume', 'destroy'] + delargs,
                 stdout = sys.stdout, stderr = sys.stderr,
                 stdin = sys.stdin,
                 cwd=self.cwd())
@@ -538,7 +606,7 @@ class BVol:
 
         dropped= (v.get_orig() for v in new)
         dropped= (v.__do_autosnap_DROP(typ, limit, "auto", "_", **A) for v in dropped)
-        dropped= flat1(dropped)
+        dropped= chain1(dropped)
         dropped= list(dropped)
 
         return (new, dropped)
@@ -571,11 +639,11 @@ class BVol:
         p_base= wrap_list(base)
         p_base= compact(base)
         p_base= ( ('-p', v.pathin_pool()) for v in base)
-        p_base= flat1(p_base)
+        p_base= chain1(p_base)
         p.push(p_base)
 
         path = self.pathin_pool()
-        cmd = itr.chain(('btrfs', 'send', path), itr.chain(p))
+        cmd = itr.chain(("echo", 'btrfs', 'send', path), itr.chain(p))
 
         stream = default(A, 'stream', )
 
@@ -603,7 +671,7 @@ class BVol:
         path = self.pathin_pool()
 
         proc = sub.Popen(
-                itr.chain(('btrfs', 'recieve'), p, path),
+                itr.chain(('echo', 'btrfs', 'recieve'), p, path),
                 stdout = sys.stdout, stderr = sys.stderr,
                 stdin = ios,
                 cwd=self.cwd())
@@ -613,7 +681,7 @@ class BVol:
     def tar_send(iot=sys.stdout, **A):
         """Creates a complex send stream with the support
         for snapshots and recursive sends.
-        If recursive!=true, this may not be a pool or prefix.
+        If recursive!=True, this may not be a pool or prefix.
 
         Args:
           iot - File object that specifies where to write
@@ -639,13 +707,6 @@ class BVol:
         if len(base) > 0:
             exp = (v for v in exp if v not in base)
 
-######################################################
-# SETUP
-
-cfg={
-    'prefix': default(os.environ, "BVOL_ROOT", "/bvol")
-}
-
 #####################################################
 # COMMANDS
 
@@ -662,34 +723,26 @@ cfg={
 # recv --plain < FILE
 # list [-s | --snaps | -S | --nosnaps] [-r | --recursive] PATH
 
-#def c_snap_auto(args):
-#def c_snap_mk(args):
-#def c_snap_destroy(args):
-#def c_sub_create(args):
-#def c_sub_destroy(args):
-#def c_send(args):
-#def c_recv(args):
-#def c_list(args):
+def common_args(m, std_prefix):
+    """The default arguments that most commands will take.
+    """
+    m.add_argument("--prefix", "-p",
+            dest="prefix",
+            default=std_prefix)
 
-def apa():
-    """Generates the argument parse used by main"""
-    m = ap.ArgumentParser(add_help = False)
+    m.add_argument("--recursive", "-r",
+            dest="recursive",
+            action="store_true",
+            default=False)
 
-    m.add_argument("--prefix", "-p", dest='prefix',
-            default=cfg['prefix'])
-
-    m.add_argument("-v", "--verbose",
-            dest='verbosity',
-            action="count")
-    m.add_argument("--verbosity",
-            dest="verbosity",
-            action="store",
-            type=int)
-    m.add_argument("--quiet", "-q",
-            dest="verbosity",
-            action="store_const",
-            const="-1")
-
+    m.add_argument("--no-snaps", "-S",
+            dest="no_snaps",
+            action="store_true",
+            default=False)
+    m.add_argument("--only-snaps", "-s",
+            dest="only_snaps",
+            action="store_true",
+            default=False)
 
     m.add_argument("--noconfirm", "--nc",
             dest="noconfirm",
@@ -704,60 +757,107 @@ def apa():
             action="store_true",
             default=False)
 
+    m.add_argument("-v", "--verbose",
+            dest='verbosity',
+            action="count")
+    m.add_argument("--verbosity",
+            dest="verbosity",
+        action="store",
+        type=int)
+    m.add_argument("--quiet", "-q",
+            dest="verbosity",
+            action="store_const",
+            const="-1")
 
-    m.add_argument("--recursive", "-r",
-            dest="recursive",
-            action="store_true",
-            default=False)
-    m.add_argument("--no-snaps", "-n",
-            dest="no_snaps",
-            action="store_true",
-            default=False)
-    m.add_argument("--only-snaps", "-s",
-            dest="only_snaps",
-            action="store_true",
-            default=False)
+def filter_snaps(vl, expect=True):
+    """Filter for snapshots. Takes a list of volumes and
+    a state to expect. For expect=True only snapshots are
+    returned. For expect=False all snapshots are omitted.
+    The return value is a iterable.
+    """
+    return ( v for v in vl if XNOR(v.is_snap(), expect))
 
-    m_ = m.add_subparsers()
-
-    # help
-    m_help = m_.add_parser('help', aliases=['h'])
-
-    m_auto = m_.add_parser('autosnap', aliases=['asnap'])
-    m_auto.add_argument("vol")
-    m_auto.add_argument("name")
-    m_auto.add_argument("keep", type=int)
-
-
-    m_mksnap = m_.add_parser('make-snap',
-            aliases=['mksnap','snap'])
-    m_mksnap.add_argument("vol")
-    m_mksnap.add_argument("name")
-
-
-    m_create = m_.add_parser("make-sub",
-            aliases=['mksub', 'sub'])
-    m_create.add_argument("vol")
-    
-    
-    m_destroy = m_.add_parser("destroy",
-            aliases = ['del', 'rm', 'remove', 'delete'])
-    m_destroy.add_argument("vol")
+def may_filter_snaps(vl, no_snaps=False, only_snaps=False):
+    """May filter snapshots: Filters if no_snaps of
+    only_snaps is set. Returns an iterable, or `vl` itself."""
+    if (no_snaps and only_snaps):
+        raise AssertionError(
+                "Can not remove AND include snapshots oO?")
+    elif (no_snaps or only_snaps):
+        return filter_snaps(vl, only_snaps)
+    else:
+        return vl
 
 
-    m_clone = m_.add_parser("clone")
-    m_clone.add_argument("src")
-    m_clone.add_argument("dest")
-    m_clone.add_argument("-ro")
+def exit_usage(reason='', code=4):
+    """Exit with the given reason and exit code, providing a
+    help message."""
+    ser.write(reason)
+    ser.write('\n')
+    exit(code)
 
-    
-    m_list = m_.add_parser("list", aliases=['ls'])
-    m_list.add_argument("vol")
+def uassert(b, reason='', code=4):
+    """Assert that `b` is True.
+    Exit using exit_usage otherwise."""
+    if (b): exit_usage(reason, code)
 
-    return m
+def cmd_help(n, args):
+    exit_usage('',0)
+
+def cmd_autosnap(n, args):
+    pass
+
+def cmd_make_snap(n, args):
+    pass
+def cmd_make_sub(n, args):
+    pass
+def cmd_destroy(n, args):
+    pass
+def cmd_clone(n, args):
+    pass
+
+def cmd_list(n, args):
+    n.add_argument("dir", default="/", nargs='?')
+    N = n.parse_args(args)
+
+    v = BVol.fromPathInPrefix(N.prefix, N.dir)
+
+    subv = None
+    if (N.recursive):
+        subv = v.subtree(recursive=True)
+    else:
+        subv = v.childs()
+
+    subv = may_filter_snaps(subv, N.no_snaps, N.only_snaps)
+    subv = sorted(subv, key=lambda v: v.pathin_fs())
+
+    print(join1(subv, delm="\n"))
+
+commands = {
+    'help': cmd_help,
+    'mksnap': cmd_make_snap,
+    'mksub': cmd_make_sub,
+    'rm': cmd_destroy,
+    'remove': cmd_destroy,
+    'clone': cmd_clone,
+    'ls': cmd_list,
+    'list': cmd_list
+}
 
 def main(argv):
-    print(apa().parse_args(argv))
+    env_prefix = default(os.environ, "BVOL_ROOT", "/bvol")
+
+    cmd = head(argv)
+    sargs = tail(argv)
+
+    n = ap.ArgumentParser(add_help = False)
+    common_args(n, env_prefix)
+
+    uassert(not cmd in commands,
+            "No such command '" + cmd + "'.")
+
+    c = commands[cmd]
+    c(n, sargs)
 
 if __name__ == "__main__":
     main(tail(sys.argv))
